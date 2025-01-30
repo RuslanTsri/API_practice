f1446f9798924dbfd47f272b068616d1b4e0a05e
ipz234_tsri <ipz234_tsri@student.ztu.edu.ua>
2025-01-30 16:20:25 +0200

    Added API_Postman_collection_documentation

--- src/routes/lawyerRoutes.js
+++ src/routes/lawyerRoutes.js
@@ -6,16 +6,12 @@ const router = express.Router();
 // Отримання всіх юристів
 router.get("/lawyers", getLawyers);
 
-// Додавання нового юриста
-router.post("/addLawyer", createLawyer);
-
 // Отримання юриста по id
 router.get("/lawyer/:id", getLawyerById);
-
-
+// Додавання нового юриста
+router.post("/addLawyer", createLawyer);
 // Оновлення інформації про юриста
 router.patch("/updateLawyer/:id", updateLawyer);
-
 // Видалення юриста
 router.delete("/deleteLawyer/:id", deleteLawyer);
 
